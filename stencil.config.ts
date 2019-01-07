75d95c4e1fc43cff9d399fa2eba90ae93fe28abc
Matthew Smith <matthew.smith@replicant.ai>
2019-01-07 12:42:32 -0500

    created simple stencil project to reproduce rollup error

---
+++ stencil.config.ts
@@ -0,0 +1,21 @@
+import { Config } from '@stencil/core';
+import builtins from 'rollup-plugin-node-builtins';
+import globals from 'rollup-plugin-node-globals';
+
+// https://stenciljs.com/docs/config
+
+export const config: Config = {
+  globalStyle: 'src/global/app.css',
+  globalScript: 'src/global/app.ts',
+  outputTargets: [
+    {
+      type: 'www',
+      // uncomment the following line to disable service workers in production
+      // serviceWorker: null
+    }
+  ],
+  plugins: [
+    builtins(),
+    globals(),
+  ]
+};
